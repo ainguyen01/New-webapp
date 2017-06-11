2a08a5e39b6542ab14576796b526c2150d0eee1b
Ai Nguyen <ainguyen01@users.noreply.github.com>
2017-06-11 00:15:12 -0500

    playing with comments and dislike in show and index

--- app/models/copay.rb
+++ app/models/copay.rb
@@ -7,6 +7,7 @@ class Copay < ApplicationRecord
   belongs_to :user, :required => false
   has_many :comments, :dependent => :destroy
   has_many :dislikes, :dependent => :destroy
+  has_many :thumbs_downs, :through => :dislikes, :source => :user
 
   validates :user, :presence => true
   validates :copay, :presence => true,:uniqueness => { :scope => [:name, :pay_as_little_as] }
